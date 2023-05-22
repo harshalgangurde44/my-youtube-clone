7ea34ed6c7aacc3bda4afbb90f480ce16897def2
Harshalgbj <125290125+Harshalgbj@users.noreply.github.com>
2023-05-22 19:27:34 +0530

    add searchbar

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useLocation } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -19,16 +20,19 @@ const WatchPage = () => {
   }
 
   return (
-    <div className="px-5">
-      <iframe
-        width="1100"
-        height="600"
-        src={`https://www.youtube.com/embed/${videoId}`}
-        title="EXTRACTION 2 | Official Hindi Trailer | Netflix India"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div>
+      <div className="px-5">
+        <iframe
+          width="1100"
+          height="600"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="EXTRACTION 2 | Official Hindi Trailer | Netflix India"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
