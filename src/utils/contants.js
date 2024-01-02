90c332e6c8620aaee7bb0ad25b3cc2e4f64c8d0c
Harshalgbj <125290125+harshalgangurde44@users.noreply.github.com>
2024-01-02 10:15:11 +0530

    add cors

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,11 +1,11 @@
 export const GOOGLE_API_KEY = "AIzaSyAUIVlSMti5p1nG_2D8bPKjlycF9lqa_s0";
 
 export const YOUTUBE_API =
-  "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key= " +
+  "https://cors-anywhere.herokuapp.com/https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key= " +
   GOOGLE_API_KEY;
 // sugestion
 export const YOUTUBE_SEARCH_API =
-  "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://cors-anywhere.herokuapp.com/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 // search-word
 // `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&key=${GOOGLE_API_KEY}&q=${searchQuery}`;
